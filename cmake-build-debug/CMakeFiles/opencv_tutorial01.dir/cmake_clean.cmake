9b916f20291cb72bcb9e0dfe24a0eafbe9b0b179
x0y14 <ya01ma08@gmail.com>
2021-07-16 21:00:12 +0900

    initial commit

---
+++ cmake-build-debug/CMakeFiles/opencv_tutorial01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_tutorial01.dir/main.cpp.o"
+  "opencv_tutorial01"
+  "opencv_tutorial01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_tutorial01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
